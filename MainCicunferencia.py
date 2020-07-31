c21adfc1824754f0b7c46793aabae75d6ef60002
SaulAlekss <rojas.alekss05@gmail.com>
2020-07-31 10:31:49 -0500

    Cilindro.py

---
+++ MainCicunferencia.py
@@ -0,0 +1,12 @@
+"""
+Nombre: Circunferencia.py
+Objetivo: 
+Autor: Saul
+Fecha: 30 de julio 2020
+"""
+from Circunferencia import Circunferencia
+
+c = Circunferencia(2,3, 10)
+print(c.toString())
+d = Circunferencia(1,10,2)
+print(d.toString())
---
+++ Punto.py
@@ -0,0 +1,34 @@
+"""
+Nombre Punto.py
+Objetivo: Muestra como trabajar en objetos con python
+Autor: Saul
+Fecha: 30 de julio 2020
+"""
+class Punto():
+
+    # Definir una especie de constructor
+    def __init__(self, valorX, valorY):
+        # definimos el nombre de los atributos del objeto o variables
+        self.x = valorX 
+        self.y = valorY
+
+    # Lista de métodos get
+    def getX(self):
+        return  self.x
+
+    def getY(self):
+        return self.y
+
+    # Lista de métodos set
+    def setX(self, valorX):
+        #asignar el argumento al atributo del objeto
+        self.x = valorX
+
+    def setY(self, valorY):
+        self.y = valorY
+
+    # Método toString: regresa los valores de los atributos x,y
+    def toString(self):
+        return "Las coordenadas X,Y del punto son:["+ str(self.getX())+","+str(self.getY())+"]"
+
+# Fin de clase
